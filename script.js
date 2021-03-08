b91b2a5068b7f89cebf2fc80e2add992ae68c345
Dhaval Kantaria <dkantaria@tekion.com>
2021-03-08 16:27:09 +0530

    Added comments and fix some indentations

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const profileContainer = document.getElementById('profileContainer');
 const postsContainer = document.getElementById("postsContainer");
 let profileData, postsData;
 
-// const posts = JSON.parse(postsData);
+// FUNCTION FOR FETCHING PROFILE DATA 
 async function fetchProfileData() {
     await fetch('./profileData.JSON')
             .then((response) => response.json())
@@ -10,11 +10,11 @@ async function fetchProfileData() {
                 profileData = data[0];
             });
 
-    // console.log(profileData);
-
+    // PROFILE PHOTO ICON IN HEADER
     const profilePhotoIcon = document.getElementById("profilePhotoIcon");
     profilePhotoIcon.src = profileData.profilePhoto;
 
+    // SHOW ALL PROFILE CONTENT
     profileContainer.innerHTML = `
         <div class="profilePhoto">
             <img src=${profileData.profilePhoto} id="profilePhoto" alt=${profileData.alt}>
@@ -37,7 +37,7 @@ async function fetchProfileData() {
     `;
 }
 
-
+// FUNCTION FOR FETCHING POSTS DATA FROM JSON FILE AND SHOW IT 
 async function fetchPostData() {
     await fetch('./postsData.JSON')
                     .then((response) => response.json())
@@ -47,41 +47,54 @@ async function fetchPostData() {
     // console.log(postData);
 
     postsData.forEach(post => {
+        // POST CONTAINER
         const postDiv = document.createElement("div");
         postDiv.classList.add("postImage");
         
+        // POST IMAGE
         const postImage = document.createElement("img");
         postImage.classList.add("post");
         postImage.src = post.postImageLink;
         postImage.alt = post.alt;
     
+        // SHOW LIKES AND COMMENTS WHEN HOVER ON IMAGE
         const likesAndCommentsDiv = document.createElement("div");
         likesAndCommentsDiv.classList.add("likesAndComments");
     
+
+        // POST HOVER CONTENT
+        // LIKES
         const postHoverLikeBtn = document.createElement("button");
         postHoverLikeBtn.classList.add("postHoverBtn");
         
+        // ICON FOR LIKE
         const postHoverLikeIcon = document.createElement("i");
         postHoverLikeIcon.classList.add("postHoverIcon");
         postHoverLikeIcon.classList.add("fas");
         postHoverLikeIcon.classList.add("fa-heart");
     
+        // NUMBER OF LIKES
         const numberOfLikes = document.createElement("span");
         numberOfLikes.innerText = post.numberOfLikes;
+        numberOfLikes.id = "numberOfLikes";
         
-    
+        // COMMENTS
         const postHoverCommentBtn = document.createElement("button");
         postHoverCommentBtn.classList.add("postHoverBtn");
         
+        // ICON FOR COMMENT
         const postHoverCommentIcon = document.createElement("i");
         postHoverCommentIcon.classList.add("postHoverIcon");
         postHoverCommentIcon.classList.add("fas");
         postHoverCommentIcon.classList.add("fa-comment");
     
+        // NUMBER OF COMMENTS
         const numberOfComments = document.createElement("span");
         numberOfComments.innerText = post.numberOfComments;
+        numberOfComments.id = "numberOfComments";
         
-        // postHoverLikeBtn.innerText = post.numberOfLikes;
+        
+        // APPEND ALL ELEMENTS TO THEIR PARENTS
         postHoverLikeBtn.appendChild(postHoverLikeIcon);
         postHoverLikeBtn.appendChild(numberOfLikes);
     
