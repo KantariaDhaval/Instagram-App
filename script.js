ec0b0f1ebf51c69452026f70d0282cb7f44da789
Dhaval Kantaria <dkantaria@tekion.com>
2021-03-08 11:50:06 +0530

    Get data using fetch from local JSON file

--- script.js
+++ script.js
@@ -1,35 +1,65 @@
 const postsContainer = document.getElementById("postsContainer");
+let postsData;
 
-const posts = JSON.parse(postsData);
-console.log(posts);
+// const posts = JSON.parse(postsData);
+async function fetchData() {
+    await fetch('./postsData.JSON')
+                    .then((respond) => respond.json())
+                    .then((data) => {
+                        postsData = data;
+                    });
+    // console.log(postData);
 
-posts.forEach(post => {
-    const postDiv = document.createElement("div");
-    postDiv.classList.add("postImage");
+    postsData.forEach(post => {
+        const postDiv = document.createElement("div");
+        postDiv.classList.add("postImage");
+        
+        const postImage = document.createElement("img");
+        postImage.classList.add("post");
+        postImage.src = post.postImageLink;
+        postImage.alt = post.alt;
     
-    const postImage = document.createElement("img");
-    postImage.classList.add("post");
-    postImage.src = post.postImageLink;
-    postImage.alt = post.alt;
+        const likesAndCommentsDiv = document.createElement("div");
+        likesAndCommentsDiv.classList.add("likes");
+    
+        const postHoverLikeBtn = document.createElement("button");
+        postHoverLikeBtn.classList.add("postHoverBtn");
+        
+        const postHoverLikeIcon = document.createElement("i");
+        postHoverLikeIcon.classList.add("postHoverIcon");
+        postHoverLikeIcon.classList.add("fas");
+        postHoverLikeIcon.classList.add("fa-heart");
+    
+        const numberOfLikes = document.createElement("span");
+        numberOfLikes.innerText = post.numberOfLikes;
+        
+    
+        const postHoverCommentBtn = document.createElement("button");
+        postHoverCommentBtn.classList.add("postHoverBtn");
+        
+        const postHoverCommentIcon = document.createElement("i");
+        postHoverCommentIcon.classList.add("postHoverIcon");
+        postHoverCommentIcon.classList.add("fas");
+        postHoverCommentIcon.classList.add("fa-comment");
+    
+        const numberOfComments = document.createElement("span");
+        numberOfComments.innerText = post.numberOfComments;
+        
+        // postHoverLikeBtn.innerText = post.numberOfLikes;
+        postHoverLikeBtn.appendChild(postHoverLikeIcon);
+        postHoverLikeBtn.appendChild(numberOfLikes);
+    
+        postHoverCommentBtn.appendChild(postHoverCommentIcon);
+        postHoverCommentBtn.appendChild(numberOfComments);
 
-    const likesAndCommentsDiv = document.createElement("div");
-    likesAndCommentsDiv.classList.add("likes");
+        likesAndCommentsDiv.appendChild(postHoverLikeBtn);
+        likesAndCommentsDiv.appendChild(postHoverCommentBtn);
+    
+        postDiv.appendChild(postImage);
+        postDiv.appendChild(likesAndCommentsDiv);
+    
+        postsContainer.appendChild(postDiv);
+    });
+}
+fetchData();
 
-    const postHoverLikeBtn = document.createElement("button");
-    postHoverLikeBtn.classList.add("postHoverBtn");
-    postHoverLikeBtn.id = "likeBtn";
-    postHoverLikeBtn.innerHTML = `<i class="postHoverIcon fas fa-heart"></i>`;
-
-    const postHoverCommentBtn = document.createElement("button");
-    postHoverCommentBtn.classList.add("postHoverBtn");
-    postHoverCommentBtn.id = "commentBtn";
-    postHoverCommentBtn.innerHTML = `<i class="postHoverIcon fas fa-comment"></i>`;
-
-    likesAndCommentsDiv.appendChild(postHoverLikeBtn);
-    likesAndCommentsDiv.appendChild(postHoverCommentBtn);
-
-    postDiv.appendChild(postImage);
-    postDiv.appendChild(likesAndCommentsDiv);
-
-    postsContainer.appendChild(postDiv);
-});
